ecfa1cb4160fd1d5c7694db57eb5bbfc54acd890
satyenderkumar97 <satyenderkumar97@gmail.com>
2016-11-08 19:14:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,16 +1,187 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
+var crypto =  require('crypto');
+var bodyParser =  require('body-Parser');
+ 
+var config = {
+    user:'amarpandeymailani',
+    database:'amarpandeymailani',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.JSON());
+var articles={
+    'article-one':{
+    title:'article-one:amar',
+    heading:'article-one',
+    date:'sep.28, 2016',
+    content:` 
+            <p>
+                this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.
+            </p>
+            <p>
+                this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.
+            </p>
+            <p>
+                this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.this is the contet of my first article.
+            </p>`
+       },
+    'article-two':{ title:'article-two:amar',
+    heading:'article-two',
+    date:'sep.29, 2016',
+    content:` 
+            <p>
+                this is the contet of my second article.this is the contet of my second article.this is the contet of my second article.
+            </p>`},
+    'article-three':{ title:'article-three:three',
+    heading:'article-three',
+    date:'sep.30, 2016',
+    content:` 
+            <p>
+                this is the contet of my third article.this is the contet of my third article.this is the contet of my third article.
+            </p>`}
+};
+    
+   function createTemplate(data){
+       var title=data.title;
+       var heading=data.heading;
+       var date=data.date;
+       var content=data.content;
+       
+     var htmlTemplate=`<html>
+     <head>
+         <title>
+             ${title}
+         </title>
+         <meta name="viewport" content="width=device-width,initial-scale=1"/>
+          <link href="/ui/style.css" rel="stylesheet" />
+        
+     </head>
+    
+    
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date.toDateString()}
+        </div>
+        <div>
+            ${content}
+            </div>
+        </div>
+     </body>
+    </html>
+   `;
+   return htmlTemplate;
+   }
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+ 
+ function hash (input,salt) {
+     //how we do craete ahash
+     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+     return ["pkdf2","10000",salt,hashed.toString('hex')].join('$');
+     
+ }
+ 
+ app.get('/hash/:input',function (req,res){
+     var hashedString = hash(req.params.input,'this-is-some-random-string');
+     res.send(hashedString);
+ });
+  
+  app.post('/create-user',function(req,res){
+      //username,password
+      //{"username:amar,"password:password"}
+      // JSON
+      var username = req.body.username;
+       var password = req.body.password;
+      
+      var salt = crypto.randomBytes(128).toString('hex');
+      var dbString = hash(password,salt);
+      pool.query('SELECT * FROM test' ,function (err,result){
+           if(err){
+           res.status(500).send(err.tostring());
+        }else{
+            res.send('user succsessfuly created: ' + username);
+        }
+      });
+  });
+      
+  
+ var pool =new Pool(config);
+app.get('/test-db', function (req, res) {
+    //make a select request 
+    //send the response
+    pool.query('SELECT * from test',function(err,result){
+        if(err){
+           res.status(500).send(err.tostring());
+        }else{
+            res.send(JSON.stringify(result.rows));
+        }
+        
+    });
+ 
+});
+
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter + 1;
+  res.send(counter.toString());
+});
+
+var names = [];
+app.get('/submit-name', function (req, res) { //url submit-name?name= xxxx
+    //get the name from the req.
+    var name = req.query.name;
+    names.push(JSON.stringify(name));
+    //json javascript object notation   
+  
+});
+
+
+app.get('/articles/:articleName', function (req, res) {
+    //articleName=article-one
+    //articles[articleName]=content object for articleone
+    
+  //select * from   article where title = '\'; DELETE where a = \'asdf'
+  pool.query("SELECT * FROM article WHERE title = $1",[req.params.articleName] ,function(err,result){
+if(err){
+    res.status(500).send(err.toString());
+    
+} else {
+      if(result.rows.length === 0){
+          res.status(404).send('article not found');
+          
+      }
+      else{
+          var articleData = result.rows[0];
+          res.send(createTemplate(articleData));
+      }
+    
+}
+  });
+  
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
